5e7d19112def6c612f59e23f2a166a79fc2f20e2
teu <te@pop-os.localdomain>
2021-06-25 09:11:37 -0300

    first commit

--- index.js
+++ index.js
@@ -13,21 +13,23 @@ client.on("message", function(message) {
     const args = commandBody.split(' ')
     const command = args.shift().toLowerCase()
 
-    if (command === "ping") {
+    if (command[0] === "ping") {
         //usage: !nerd ping
         //comando simples que ve o ping do message.author
         const timeTaken = Date.now() - message.createdTimestamp;
         message.reply(`vai se foder mamaco filho da puta; seu ping é ${timeTaken}ms.`)
     }
-    if (command === "adicionar" && message.member.roles.cache.some(role => role.name === 'parlamento')) {
+    if (command[0] === "adicionar" && message.member.roles.cache.some(role => role.name === 'parlamento')) {
 
         //usage: !nerd adicionar ...,...,..., 
         //comando que somente ADMs podem dar para adicionar tarefas novas ao banco de dados
         
     }
-    if (command === "remover" && message.member.roles.cache.some(role => role.name === 'parlamento')) {
-        let params = commandBody.slice("remover")
-        
+    if (command[0] === "remover" && message.member.roles.cache.some(role => role.name === 'parlamento')) {
+        let params = commandBody.slice("remover").split(',');
+        message.reply(`matéria: ${params[0]}\ntítulo da tarefa:${params[1]}\ndescrição da matéria:`)
+
+
     }
     if (command === "tarefas"){
 
